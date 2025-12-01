0d2b605fbee715319292e55fb7ba6e48ebd01ab9
Bautibzn <bautibzn06@gmail.com>
2025-12-01 11:42:14 -0300

    Update modelo.py

--- programas/modelo.py
+++ programas/modelo.py
@@ -4,24 +4,22 @@ model = YOLO("yolov11n-face.pt")
 
 results = model.train(
     data="data.yaml",
-    epochs=15,          # más estable que 15 (no es mucho más lento)
-    imgsz=384,          # ideal para iGPU
-    batch=4,            # seguro para tu VRAM
+    epochs=15,          
+    imgsz=384,          
+    batch=4,           
     name='IdFace',
     amp=True,
     cos_lr=True,
-    cache='disk',       # evita saturar RAM
+    cache='disk',       
     workers=0,
-
-    mosaic=0.1,         # augment suave bueno para rostros
+    mosaic=0.1,         
     mixup=0.0,
-    hsv_h=0.015,        # augment muy controlado
+    hsv_h=0.015,        
     hsv_s=0.7,
     hsv_v=0.4,
-
-    freeze=10,          # acelera entrenamiento
+    freeze=10,         
     warmup_epochs=2,
 )
 
-# cargar mejor modelo
 model = YOLO("runs/detect/FaceId4/weights/best.pt")
+
